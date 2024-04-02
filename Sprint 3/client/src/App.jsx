5c52230092296cfc63c27d037c0662fdcf337f6c
Al Rafi Ahmed <81585085+a1rafi@users.noreply.github.com>
2024-04-03 00:30:29 +0600

    Sprint3 Completed

---
+++ Sprint 3/client/src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/createListing';
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
+import Search from './pages/Search';
+
+export default function App() {
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<Signin />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/search" element={<Search />} />
+
+    <Route path="/listing/:listingId" element={<Listing />} />
+
+    <Route element ={<PrivateRoute/>} > 
+      <Route path="/profile" element={<Profile />} />
+      <Route path='/create-listing' element={<CreateListing/>}/>
+      <Route path='/update-listing/:listingId' element={<UpdateListing />}
+    />
+  </Route>
+   </Routes>
+  </BrowserRouter>;
+}
